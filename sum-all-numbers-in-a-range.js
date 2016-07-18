f789662d7aadab870969a9e1b0b1a79fc002bb87
Dominic <haakym@gmail.com>
2016-07-18 20:04:21 +0100

    Update sum-all-numbers-in-a-range.js

--- sum-all-numbers-in-a-range.js
+++ sum-all-numbers-in-a-range.js
@@ -1,5 +1,7 @@
 // https://www.freecodecamp.com/challenges/sum-all-numbers-in-a-range
 
+// refactored into one method
+
 function sumAll(arr) {
   if(arr[0] === arr[1]) 
     return arguments[1] + Math.min.apply(null, arr) - 1;
@@ -9,4 +11,21 @@ function sumAll(arr) {
     arguments[1] ? (arguments[1] + Math.min.apply(null, arr)) : (Math.min.apply(null, arr) + 1)
   );
   
+}
+
+// original
+
+'esversion:6';
+
+function range(from, to, arr) {
+    if (from > to) return arr;
+
+    return range(from + 1, to, arr.concat(from));
+}
+
+function sumAll(arr) {
+    return range(Math.min(...arr), Math.max(...arr), [])
+        .reduce(function(previousValue, currentValue) {
+            return previousValue + currentValue;
+        });
 }
